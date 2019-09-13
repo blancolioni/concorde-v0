ed48c2700d0934204c8dad7371cff8a9cd5b19c1
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-09-13 15:51:41 +0200

    Restored installed kit

--- kit.sh
+++ kit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd src/kit
-#kit ../../config/kit/concorde.kit
-../../../kit/build/bin/kit-driver ../../config/kit/concorde.kit
+kit ../../config/kit/concorde.kit
+#../../../kit/build/bin/kit-driver ../../config/kit/concorde.kit
