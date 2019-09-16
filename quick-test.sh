cf1f051f16435882f319ecd9ba59a7e45aeb8ee0
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-09-16 08:41:22 +0200

    quick-test.sh: use --batch-mode instead of --command-line

--- quick-test.sh
+++ quick-test.sh
@@ -2,4 +2,4 @@
 ./build/bin/concorde-driver --create
 #./build/bin/concorde-driver --add-faction --account-name=root --faction-name=Republic --faction-adjective=Republican --faction-plural=Republicans --faction-color=#003153 --faction-setup-path=capital
 ./build/bin/concorde-driver --add-faction --faction-names-file=faction-names.config --faction-colors-file=faction-colors.config
-./build/bin/concorde-driver --command-line
+./build/bin/concorde-driver --batch-mode
