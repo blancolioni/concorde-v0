93eb0b99239402d6409debd3e5de7e96f6770e11
Fraser Wilson <fraser@thewhitelion.eu>
2020-03-02 19:05:09 +0100

    WIndows scripts

---
+++ quick-test.bat
@@ -0,0 +1,4 @@
+@build\bin\concorde-driver --create
+@rem ./build/bin/concorde-driver --add-faction --account-name=root --faction-name=Republic --faction-adjective=Republican --faction-plural=Republicans --faction-color=#003153 --faction-setup-path=capital
+@build\bin\concorde-driver --add-faction --faction-names-file=faction-names.config --faction-colors-file=faction-colors.config
+@build\bin\concorde-driver --batch-mode --update-count=100
