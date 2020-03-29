df5bb997714bfcc1206c713d3bcdf38aecaa03ad
Fraser Wilson <fwilson@anago.nl>
2020-03-29 21:29:23 +0200

    World model: Draw borders and owner colors

--- src/ui/models/concorde-ui-models-world.adb
+++ src/ui/models/concorde-ui-models-world.adb
@@ -10,8 +10,10 @@ with Concorde.Real_Images;
 
 with Concorde.Worlds;
 
+with Concorde.Handles.Has_Color;
+with Concorde.Handles.Terrain;
+
 with Concorde.Db.Colony;
-with Concorde.Db.Terrain;
 with Concorde.Db.World_Sector;
 
 package body Concorde.UI.Models.World is
@@ -24,11 +26,11 @@ package body Concorde.UI.Models.World is
 
    type Sector_Model is
       record
-         Owned    : Boolean;
+         Owner    : Concorde.Handles.Faction.Faction_Handle;
          Sector   : Concorde.Db.World_Sector_Reference;
          Centre   : Concorde.Worlds.Sector_Vertex;
          Boundary : Vertex_Holders.Holder;
-         R, G, B  : Unit_Real;
+         Color    : Nazar.Colors.Nazar_Color;
       end record;
 
    package Sector_Model_Vectors is
@@ -54,6 +56,14 @@ package body Concorde.UI.Models.World is
      (Model  : in out World_Model_Type'Class;
       Centre : Concorde.Worlds.Sector_Vertex);
 
+   function To_Nazar_Color
+     (Has_Color : Concorde.Handles.Has_Color.Has_Color_Class)
+      return Nazar.Colors.Nazar_Color
+   is (Red   => Nazar.Nazar_Unit_Float (Has_Color.Red),
+       Green => Nazar.Nazar_Unit_Float (Has_Color.Green),
+       Blue  => Nazar.Nazar_Unit_Float (Has_Color.Blue),
+       Alpha => 1.0);
+
    function Image (X : Real) return String
                    renames Concorde.Real_Images.Approximate_Image;
 
@@ -76,102 +86,105 @@ package body Concorde.UI.Models.World is
          "long-0 = "
          & Image (180.0 * Long_0 / Pi));
 
-      for Sector of Model.Sectors loop
-         declare
-            Color : constant Nazar.Colors.Nazar_Color :=
-                      (Nazar.Nazar_Unit_Float (Sector.R),
-                       Nazar.Nazar_Unit_Float (Sector.G),
-                       Nazar.Nazar_Unit_Float (Sector.B),
-                       1.0);
-            Boundary : constant Concorde.Worlds.Sector_Vertex_Array :=
-                         Sector.Boundary.Element;
-            Centre_Long : Real :=
-                            Arctan (Sector.Centre.Y, Sector.Centre.X)
-                            - Long_0;
-            First    : Boolean := True;
-         begin
-            if Centre_Long < -Pi then
-               Centre_Long := Centre_Long + 2.0 * Pi;
-            elsif Centre_Long > Pi then
-               Centre_Long := Centre_Long - 2.0 * Pi;
-            end if;
+      for Fill in reverse Boolean loop
 
-            Concorde.Logging.Log
-              (Actor    => "model",
-               Location => "world",
-               Category => "draw",
-               Message  =>
-                 "centre = ("
-               & Image (Sector.Centre.X)
-               & ","
-               & Image (Sector.Centre.Y)
-               & ","
-               & Image (Sector.Centre.Z)
-               & "); lat/long = ("
-               & Image (Arcsin (Sector.Centre.Z) * 180.0 / Pi)
-               & ","
-               & Image (Centre_Long)
-               & ")");
+         Model.Set_Fill (Fill);
 
-            if True
-              or else (Centre_Long in -Pi / 2.0 .. Pi / 2.0
-                       and then Sector.Centre.Z >= 0.0)
-            then
-               Model.Set_Color (Color);
-               Model.Set_Fill (True);
+         if not Fill then
+            Model.Set_Color ((0.0, 0.0, 0.0, 1.0));
+         end if;
+
+         for Sector of Model.Sectors loop
+            declare
+               Color       : constant Nazar.Colors.Nazar_Color :=
+                               Sector.Color;
+               Boundary    : constant Concorde.Worlds.Sector_Vertex_Array :=
+                               Sector.Boundary.Element;
+               Centre_Long : Real :=
+                               Arctan (Sector.Centre.Y, Sector.Centre.X)
+                               - Long_0;
+               First       : Boolean := True;
+            begin
+               if Centre_Long < -Pi then
+                  Centre_Long := Centre_Long + 2.0 * Pi;
+               elsif Centre_Long > Pi then
+                  Centre_Long := Centre_Long - 2.0 * Pi;
+               end if;
+
+--                 Concorde.Logging.Log
+--                   (Actor    => "model",
+--                    Location => "world",
+--                    Category => "draw",
+--                    Message  =>
+--                      "centre = ("
+--                    & Image (Sector.Centre.X)
+--                    & ","
+--                    & Image (Sector.Centre.Y)
+--                    & ","
+--                    & Image (Sector.Centre.Z)
+--                    & "); lat/long = ("
+--                    & Image (Arcsin (Sector.Centre.Z) * 180.0 / Pi)
+--                    & ","
+--                    & Image (Centre_Long)
+--                    & ")");
+
+               if Fill then
+                  Model.Set_Color (Color);
+               end if;
+
                for Pt of Boundary loop
                   declare
                      use Nazar;
-                     Long   : constant Real :=
-                                Arctan (Pt.Y, Pt.X) - Long_0;
-                     Lat    : constant Real := Arcsin (Pt.Z);
+                     Long    : constant Real :=
+                                 Arctan (Pt.Y, Pt.X) - Long_0;
+                     Lat     : constant Real := Arcsin (Pt.Z);
                      Cos_Lat : constant Nazar_Float :=
                                  Nazar_Float (Cos (Lat));
-                     Map_Pt : Map_Point :=
-                                Project (Lat, Long);
+                     Map_Pt  : Map_Point :=
+                                 Project (Lat, Long);
                   begin
-                     Concorde.Logging.Log
-                       (Actor    => "model",
-                        Location => "world",
-                        Category => "draw",
-                        Message  =>
-                          "    pt = ("
-                        & Image (Pt.X)
-                        & ","
-                        & Image (Pt.Y)
-                        & ","
-                        & Image (Pt.Z)
-                        & "); lat/long = ("
-                        & Image (Lat * 180.0 / Pi)
-                        & ","
-                        & Image (Long)
-                        & ")"
-                        & "; map pt = ("
-                        & Image (Real (Map_Pt.X))
-                        & ","
-                        & Image (Real (Map_Pt.Y))
-                        & ")");
-
+--                       Concorde.Logging.Log
+--                         (Actor    => "model",
+--                          Location => "world",
+--                          Category => "draw",
+--                          Message  =>
+--                            "    pt = ("
+--                          & Image (Pt.X)
+--                          & ","
+--                          & Image (Pt.Y)
+--                          & ","
+--                          & Image (Pt.Z)
+--                          & "); lat/long = ("
+--                          & Image (Lat * 180.0 / Pi)
+--                          & ","
+--                          & Image (Long)
+--                          & ")"
+--                          & "; map pt = ("
+--                          & Image (Real (Map_Pt.X))
+--                          & ","
+--                          & Image (Real (Map_Pt.Y))
+--                          & ")");
+--
                      if Centre_Long < 0.0
                        and then Real (Map_Pt.X) > Pi / 2.0 - abs Lat
                      then
                         Map_Pt.X := Map_Pt.X - 4.0 * Sqrt_2 * Cos_Lat;
-                        Concorde.Logging.Log
-                          (Actor    => "model",
-                           Location => "world",
-                           Category => "draw",
-                           Message  =>
-                             "    move x to " & Image (Real (Map_Pt.X)));
+--                          Concorde.Logging.Log
+--                            (Actor    => "model",
+--                             Location => "world",
+--                             Category => "draw",
+--                             Message  =>
+--                               "    move x to " & Image (Real (Map_Pt.X)));
                      elsif Centre_Long > 0.0
                        and then Real (Map_Pt.X) < -Pi / 2.0 + abs Lat
                      then
                         Map_Pt.X := Map_Pt.X + 4.0 * Sqrt_2 * Cos_Lat;
-                        Concorde.Logging.Log
-                          (Actor    => "model",
-                           Location => "world",
-                           Category => "draw",
-                           Message  =>
-                             "    move x to " & Image (Real (Map_Pt.X)));
+--                          Concorde.Logging.Log
+--                            (Actor    => "model",
+--                             Location => "world",
+--                             Category => "draw",
+--                             Message  =>
+--                               "    move x to " & Image (Real (Map_Pt.X)));
                      end if;
 
                      if First then
@@ -183,8 +196,8 @@ package body Concorde.UI.Models.World is
                   end;
                end loop;
                Model.Render;
-            end if;
-         end;
+            end;
+         end loop;
       end loop;
    end Draw_World;
 
@@ -217,7 +230,7 @@ package body Concorde.UI.Models.World is
                    (2.0 * Theta + Sin (2.0 * Theta) - Pi_Sin_Lat)
                      / (2.0 + 2.0 * Cos (2.0 * Theta));
 
-               exit when abs (Prev - Theta) < 0.0001;
+               exit when abs (Prev - Theta) < 1.0e-9;
             end;
          end loop;
       end if;
@@ -246,23 +259,26 @@ package body Concorde.UI.Models.World is
         Concorde.Db.World_Sector.Select_By_World (World.Reference_World)
       loop
          declare
-            use Concorde.Db;
             Ref : constant Concorde.Db.World_Sector_Reference :=
                     Sector.Get_World_Sector_Reference;
-            Terrain : constant Concorde.Db.Terrain.Terrain_Type :=
-                        Concorde.Db.Terrain.Get (Sector.Terrain);
+            Terrain : constant Concorde.Handles.Terrain.Terrain_Handle :=
+                        Concorde.Handles.Terrain.Get (Sector.Terrain);
+            Faction : constant Concorde.Handles.Faction.Faction_Handle :=
+                        Concorde.Handles.Faction.Get (Sector.Faction);
+            Color   : constant Nazar.Colors.Nazar_Color :=
+                        (if Faction.Has_Element
+                         then To_Nazar_Color (Faction)
+                         else To_Nazar_Color (Terrain));
          begin
             Result.Sectors.Append
               (Sector_Model'
                  (Sector   => Ref,
-                  Owned    => Sector.Faction /= Null_Faction_Reference,
+                  Owner    => Concorde.Handles.Faction.Get (Sector.Faction),
                   Centre   => Concorde.Worlds.Get_Centre (Ref),
                   Boundary =>
                     Vertex_Holders.To_Holder
                       (Concorde.Worlds.Get_Vertices (Ref)),
-                  R        => Terrain.Red,
-                  G        => Terrain.Green,
-                  B        => Terrain.Blue));
+                  Color    => Color));
          end;
       end loop;
 
